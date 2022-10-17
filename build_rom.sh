a2f62187c18b4f071ffacb5ce83867816bea6f15
pjht <terppa05@gmail.com>
2022-10-17 10:42:38 -0500

    Change ROM build to use GNU binutils

--- build_rom.sh
+++ build_rom.sh
@@ -1,2 +1,5 @@
-vasmm68k_std rom.68k -o rom.bin -Fbin
+#! /bin/bash -x
+m68k-elf-as -m68010 --register-prefix-optional -o rom.o rom.68k
+m68k-elf-ld -z max-page-size=1 -T rom.ld -o rom.elf rom.o
+m68k-elf-objcopy -O binary rom.elf rom.bin
 
